09da4d49d50250365f90f3f6e16f14fe297107ec
prakhar saxena <prakharsaxenapsgmid@gmail.com>
2026-07-16 17:42:30 +0000

    Sync LeetCode submission Runtime - 526 ms (95.14%), Memory - 0.0B (100.00%)

--- leetcode-solutions/0183-customers-who-never-order/solution.sql
+++ leetcode-solutions/0183-customers-who-never-order/solution.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-select name as Customers from Customers left join Orders on Customers.id=Orders.customerid
-where Orders.customerId IS NULL
+select name as Customers from Customers left join Orders on Customers.id=Orders.Customerid where Orders.CustomerId is null;
