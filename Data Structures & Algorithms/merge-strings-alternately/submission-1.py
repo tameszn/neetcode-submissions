981c34447154ce5b30f57e7689a0e8c9715bff66
prakhar saxena <prakharsaxenapsgmid@gmail.com>
2026-08-08 23:52:55 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+       i=j=0
+       merged=""
+       while i<len(word1) and j<len(word2):
+          merged+=word1[i]+word2[j]
+          i+=1
+          j+=1
+       if(i<len(word1)):
+          merged+=word1[i:]
+       else:
+          merged+=word2[j:]
+       return merged   
