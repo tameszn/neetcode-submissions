559a5c362d3bf02b4908e08354d8ab2b0c2b0172
prakhar saxena <prakharsaxenapsgmid@gmail.com>
2026-08-28 11:27:34 +0000

    Sync LeetCode submission Runtime - 287 ms (89.09%), Memory - 0.0B (100.00%)

--- leetcode-solutions/0595-big-countries/solution.sql
+++ leetcode-solutions/0595-big-countries/solution.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select name,population,area  from World where World.area>=3000000 or world.population>=25000000;
+select name,area,population from World where population>=25000000 or area>=3000000;
+
