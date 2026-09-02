e8911fbe9c420b0d58171d8c3fc76572b494c9f4
prakhar saxena <prakharsaxenapsgmid@gmail.com>
2026-09-02 05:33:05 +0000

    Sync LeetCode submission Runtime - 438 ms (79.27%), Memory - 0.0B (100.00%)

--- leetcode-solutions/0619-biggest-single-number/solution.sql
+++ leetcode-solutions/0619-biggest-single-number/solution.sql
@@ -1,8 +1,2 @@
-# Write your MySQL query statement beselect max(num) as num
-select max(num) as num
-from(
-    select num
-    from mynumbers
-    group by num
-    having count(num)=1
-)as num
+
+select max(num) as num from MyNumbers where num in (select num from MyNumbers group by num having count(*)<=1 );
