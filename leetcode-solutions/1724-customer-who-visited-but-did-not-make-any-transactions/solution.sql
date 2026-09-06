73367172e50f11fd392869856204fac0a724a9de
prakhar saxena <prakharsaxenapsgmid@gmail.com>
2026-09-06 13:18:41 +0000

    Sync LeetCode submission Runtime - 1473 ms (63.58%), Memory - 0.0B (100.00%)

---
+++ leetcode-solutions/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT customer_id, COUNT(visit_id) as count_no_trans 
+FROM Visits v
+WHERE NOT EXISTS (
+	SELECT visit_id FROM Transactions t 
+	WHERE t.visit_id = v.visit_id
+	)
+GROUP BY customer_id
+
+
